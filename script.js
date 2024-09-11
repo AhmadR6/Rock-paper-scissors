d5ec721c4be7e3e4fca9aa3eb4d067081e44356d
Ahmad Rehman <ahmadrehman851@gmail.com>
2024-09-12 00:38:13 +0200

    added playRound funtion.

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
 const choices = ["rock", "paper", "scissors"];
 const getComputerChoice = () => {
   const computerChoice = choices[Math.floor(Math.random() * 3)];
-  console.log(computerChoice);
+
+  return computerChoice;
 };
-getComputerChoice();
 
 const getHumanChoice = () => {
-  const humanChoice = prompt("Your choice : ");
-  console.log(humanChoice);
+  const humanChoice = prompt(
+    "Your choice (rock, paper, scissors): "
+  ).toLowerCase();
+
+  return humanChoice;
 };
-getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+const playRound = (humanChoice, computerChoice) => {
+  if (humanChoice === computerChoice) {
+    console.log("Its a tie");
+    return;
+  }
+  if (humanChoice === "rock" && computerChoice === "scissors") {
+    console.log("You win! Rock beats scissor");
+    humanScore += 1;
+    return;
+  }
+  if (humanChoice === "scissors" && computerChoice === "paper") {
+    console.log("You win! scissors beats paper");
+    humanScore += 1;
+    return;
+  }
+  if (humanChoice === "paper" && computerChoice === "rock") {
+    console.log("You win! paper beats rock");
+    humanScore += 1;
+    return;
+  } else {
+    console.log(`you lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore += 1;
+  }
+};
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
